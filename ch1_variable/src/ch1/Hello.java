47d83d0192a3e731652ba7ebafd943b6a6b41cf2
dudgus1286 <dudgus1286@gmail.com>
2024-01-30 17:50:41 +0900

    클래스 작성

---
+++ ch1_variable/src/ch1/Hello.java
@@ -0,0 +1,14 @@
+package ch1;
+
+// 클래스 이름은 대문자로 시작
+// 클래스명 한글 가능하나 되도록이면 영어로 사용
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("안녕하세요");
+        System.out.println("안녕하세요");
+        System.out.println("안녕하세요");
+        System.out.println("안녕하세요");
+        System.out.println("안녕하세요");
+        System.out.println("안녕하세요");
+    }
+}
---
+++ ch1_variable/src/ch1/Hello2.java
@@ -0,0 +1,9 @@
+package ch1;
+
+public class Hello2 {
+    public static void main(String[] args) {
+        int val1 = 12 ;
+        System.out.println("내가 좋아하는 숫자는 " + val1);
+    }
+
+}
